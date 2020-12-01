d660b283483cc0e5e11cddafcbf7bed532115531
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-12-01 11:51:39 -0700

    Implement removing blocks

--- src/main.zig
+++ src/main.zig
@@ -204,8 +204,116 @@ pub fn onEvent(context: *platform.Context, event: platform.event.Event) !void {
             if (camera_angle.y > std.math.pi / 2.0)
                 camera_angle.y = std.math.pi / 2.0;
         },
+        .MouseButtonDown => |click| switch (click.button) {
+            .Left => {
+                const CX = core.chunk.CX;
+                const CY = core.chunk.CY;
+                const CZ = core.chunk.CZ;
+
+                const lookat = vec3f(std.math.sin(camera_angle.x) * std.math.cos(camera_angle.y), std.math.sin(camera_angle.y), std.math.cos(camera_angle.x) * std.math.cos(camera_angle.y));
+                const start = cam_position;
+                const end = cam_position.addv(lookat.scale(5));
+
+                var iterations_left: usize = 100;
+                var voxel_iter = VoxelTraversal.init(start, end);
+                while (voxel_iter.next()) |voxel_pos| {
+                    if (iterations_left == 0) break;
+                    iterations_left -= 1;
+
+                    if (voxel_pos.x < 0 or voxel_pos.y < 0 or voxel_pos.z < 0) continue;
+                    if (voxel_pos.x >= CX or voxel_pos.y >= CY or voxel_pos.z >= CZ) continue;
+
+                    const chunk_pos = voxel_pos.intCast(u8);
+                    const block = chunkRender.chunk.blk[chunk_pos.x][chunk_pos.y][chunk_pos.z];
+                    if (block == .Air) continue;
+
+                    // Break block
+                    chunkRender.chunk.blk[chunk_pos.x][chunk_pos.y][chunk_pos.z] = .Air;
+                    chunkRender.update();
+                    break;
+                }
+            },
+            else => {},
+        },
         else => {},
     }
+}
+
+const VoxelTraversal = struct {
+    current_voxel: math.Vec(3, i32),
+    last_voxel: math.Vec(3, i32),
+    step: math.Vec(3, i32),
+    tMax: Vec3f,
+    tDelta: Vec3f,
+    returned_last_voxel: bool = false,
+
+    pub fn init(start: Vec3f, end: Vec3f) @This() {
+        const current_voxel = start.floor().floatToInt(i32);
+        const direction = end.subv(start);
+        const step = math.Vec(3, i32){
+            .x = if (direction.x >= 0) 1 else -1,
+            .y = if (direction.y >= 0) 1 else -1,
+            .z = if (direction.z >= 0) 1 else -1,
+        };
+        const next_voxel_boundary = math.Vec3f{
+            .x = @intToFloat(f32, current_voxel.x + step.x),
+            .y = @intToFloat(f32, current_voxel.y + step.y),
+            .z = @intToFloat(f32, current_voxel.z + step.z),
+        };
+        return @This(){
+            .current_voxel = current_voxel,
+            .last_voxel = end.floor().floatToInt(i32),
+            .step = step,
+            .tMax = math.Vec3f{
+                .x = if (direction.x != 0) (next_voxel_boundary.x - start.x) / direction.x else std.math.f32_max,
+                .y = if (direction.y != 0) (next_voxel_boundary.y - start.y) / direction.y else std.math.f32_max,
+                .z = if (direction.z != 0) (next_voxel_boundary.z - start.z) / direction.z else std.math.f32_max,
+            },
+            .tDelta = math.Vec3f{
+                .x = if (direction.x != 0) 1.0 / direction.x * @intToFloat(f32, step.x) else std.math.f32_max,
+                .y = if (direction.y != 0) 1.0 / direction.y * @intToFloat(f32, step.y) else std.math.f32_max,
+                .z = if (direction.z != 0) 1.0 / direction.z * @intToFloat(f32, step.z) else std.math.f32_max,
+            },
+        };
+    }
+
+    pub fn next(this: *@This()) ?math.Vec(3, i32) {
+        if (this.last_voxel.eql(this.current_voxel)) {
+            if (this.returned_last_voxel) {
+                return null;
+            } else {
+                this.returned_last_voxel = true;
+                return this.last_voxel;
+            }
+        }
+        if (this.tMax.x < this.tMax.y) {
+            if (this.tMax.x < this.tMax.z) {
+                this.current_voxel.x += this.step.x;
+                this.tMax.x += this.tDelta.x;
+            } else {
+                this.current_voxel.z += this.step.z;
+                this.tMax.z += this.tDelta.z;
+            }
+        } else {
+            if (this.tMax.y < this.tMax.z) {
+                this.current_voxel.y += this.step.y;
+                this.tMax.y += this.tDelta.y;
+            } else {
+                this.current_voxel.z += this.step.z;
+                this.tMax.z += this.tDelta.z;
+            }
+        }
+        return this.current_voxel;
+    }
+};
+
+fn raycast(origin: Vec3f, dir: Vec3f) ?math.Vec3i {
+    var pos = origin.floatToInt(i32);
+    const step = math.Vec3i{
+        .x = if (dir.x > 0) 1 else -1,
+        .y = if (dir.y > 0) 1 else -1,
+        .z = if (dir.z > 0) 1 else -1,
+    };
 }
 
 pub fn update(context: *platform.Context, current_time: f64, delta: f64) !void {
